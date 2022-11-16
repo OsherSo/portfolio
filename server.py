e93ca9f70a40f2aa921646b2b4a2954dbba1f042
Osher Solimany <solimani.osher@gmail.com>
2022-11-16 14:45:17 +0200

    Updates

--- server.py
+++ server.py
@@ -13,9 +13,5 @@ def html_page(page_name):
     return render_template(page_name)
 
 
-@app.route("/submit_form", methods=["POST"])
-def submit_form():
-    if request.method == "POST":
-        data = request.form.to_dict()
-        print(data)
-        return redirect("thankyou.html")
+if __name__ == "__main__":
+    app.run(debug=True)
