fb2630fe46bbfc4e8b016f1af4e637eab9f8ed8d
Osher <solimani.osher@gmail.com>
2024-07-30 17:33:39 +0300

    add trade-diary

--- src/data.jsx
+++ src/data.jsx
@@ -56,4 +56,12 @@ export const projects = [
     title: "Job Tracker",
     text: "Welcome to our job tracking app, where you can organize your job hunt like a pro. Discover and apply for opportunities seamlessly while staying on top of your applications. Gain insights and manage your career path effectively with our intuitive tools and resources.",
   },
+  {
+    id: nanoid(),
+    img: "https://golygrail.com/wp-content/uploads/2023/12/trade_diary.png",
+    url: "https://trade-diary.onrender.com/",
+    github: "https://github.com/OsherSo/trade-diary",
+    title: "TradeDiary",
+    text: "TradeDiary is a comprehensive trading journal application that helps traders track and analyze their trades. With features like trade logging, performance analytics, and strategy refinement insights, TradeDiary empowers traders to make data-driven decisions and improve their trading strategies.",
+  },
 ];
