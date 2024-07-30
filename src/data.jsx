c2f8de3399d4580650d469237e00abd9c0a37634
Osher <solimani.osher@gmail.com>
2024-07-30 17:27:50 +0300

    add job-tracker

--- src/data.jsx
+++ src/data.jsx
@@ -51,7 +51,7 @@ export const projects = [
   {
     id: nanoid(),
     img: "https://www.ec2i.biz/hubfs/Job%20Tracking.png",
-    url: "https://job-tracker-966u.onrender.com",
+    url: "https://job-tracker-70md.onrender.com/",
     github: "https://github.com/OsherSo/job-tracker",
     title: "Job Tracker",
     text: "Welcome to our job tracking app, where you can organize your job hunt like a pro. Discover and apply for opportunities seamlessly while staying on top of your applications. Gain insights and manage your career path effectively with our intuitive tools and resources.",
